71e059dbbde5ab69c2d1830f77c6c09184fa195c
Dipak Ugale <ugale.deepak3010@gmail.com>
2024-10-10 16:31:32 +0000

    initialize

---
+++ thyemeleafexample/src/main/java/com/thymeleafexample/thyemeleafexample/ThyemeleafexampleApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleafexample.thyemeleafexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyemeleafexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyemeleafexampleApplication.class, args);
+	}
+
+}
---
+++ thyemeleafexample/src/test/java/com/thymeleafexample/thyemeleafexample/ThyemeleafexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleafexample.thyemeleafexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyemeleafexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
